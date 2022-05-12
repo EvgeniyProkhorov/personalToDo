b2b43c845ed8baed2d1af75c253a11a91401af0a
EvgeniyProkhorov <firerequest@gmail.com>
2022-05-12 22:08:24 +0300

    react-router-dom and login

---
+++ src/Components/404/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const NotFound = () => {
+    return (
+        <h1 style={{textAlign: 'center'}}>
+            Page Not Found!
+        </h1>
+    )
+}
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,16 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import {TodolistsList} from "../Components/TodolistsList/TodolistsList";
 import {Login} from "../Components/Auth/Login/Login";
+import {NotFound} from "../Components/404/NotFound";
 
 function App() {
     return (
         <Routes>
             <Route path='/' element={<TodolistsList/>}/>
             <Route path='login' element={<Login/>}/>
+            <Route path='*' element={<NotFound/>}/>
         </Routes>
     )
 }
+
 export default App;
